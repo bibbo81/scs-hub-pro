e8c4bf6382f307e12e7755f3e4175a989e571c97
bibb81 <fabrizio.cagnucci@gmail.com>
2025-07-10 01:10:12 +0200

    Fix UUID input syntax error

--- core/shipments-registry.js
+++ core/shipments-registry.js
@@ -601,7 +601,14 @@ class ShipmentsRegistry {
     // ===== UTILITY METHODS =====
     
     generateId() {
-        return 'SHIP-' + Date.now() + '-' + Math.random().toString(36).substr(2, 9);
+        if (typeof crypto !== 'undefined' && crypto.randomUUID) {
+            return crypto.randomUUID();
+        }
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
+            const r = Math.random() * 16 | 0;
+            const v = c === 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        });
     }
     
     generateShipmentNumber() {
